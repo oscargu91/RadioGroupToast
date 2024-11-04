258376aa1d350eb920d80b677b7d3c61b0125e5a
DAMDU211 <oscargulin@hotmail.com>
2024-11-04 20:04:02 +0100

    Commit final

--- app/src/main/java/com/example/radiogrouptoast/MainActivity.java
+++ app/src/main/java/com/example/radiogrouptoast/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.radiogrouptoast;
 
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.ForegroundColorSpan;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.graphics.Color;
@@ -25,29 +30,47 @@ public class MainActivity extends AppCompatActivity {
         // EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        // Referencias a los botones de colores
-        Button redButton = findViewById(R.id.botonRojo);
-        Button blueButton = findViewById(R.id.botonAzul);
-        Button greenButton = findViewById(R.id.botonVerde);
-        Button yellowButton = findViewById(R.id.botonAmarillo);
 
-        // Configura el comportamiento para cada botón de color
-        redButton.setOnClickListener(v -> mostrarToastColor("Rojo", Color.RED));
-        blueButton.setOnClickListener(v -> mostrarToastColor("Azul", Color.BLUE));
-        greenButton.setOnClickListener(v -> mostrarToastColor("Verde", Color.GREEN));
-        yellowButton.setOnClickListener(v -> mostrarToastColor("Amarillo", Color.YELLOW));
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+
+        });
+
+        RadioGroup grupo = findViewById(R.id.radioGrupo);
+
+        grupo.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                RadioButton boton = findViewById(grupo.getCheckedRadioButtonId());
+
+                SpannableStringBuilder cadena = new SpannableStringBuilder();
+                if (boton.getText().equals("Rojo")) {
+                    cadena.append("Rojo");
+                    cadena.setSpan(new ForegroundColorSpan(Color.RED), 0, cadena.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                } else if (boton.getText().equals("Azul")) {
+                    cadena.append("Azul");
+                    cadena.setSpan(new ForegroundColorSpan(Color.BLUE), 0, cadena.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                } else {
+                    cadena.append("Verde");
+                    cadena.setSpan(new ForegroundColorSpan(Color.GREEN), 0, cadena.length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                }
+
+                Toast.makeText(MainActivity.this, cadena, Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
     }
-
-    // Método para mostrar el Toast con el nombre y el color del texto seleccionado
-    private void mostrarToastColor(String nombreColor, int color) {
-        Toast toast = Toast.makeText(getApplicationContext(), nombreColor, Toast.LENGTH_SHORT);
-        View view = toast.getView();
-
-        // Cambia el color del texto del Toast
-        view.findViewById(android.R.id.message).setBackgroundColor(Color.TRANSPARENT);
-        ((TextView) view.findViewById(android.R.id.message)).setTextColor(color);
-
-        toast.show();
-    };
 }
+
+
+
+
+
 
